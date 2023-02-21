28fdd081f96d39bacb66e863b101f3a0a7a3fc95
tahirgngr1 <112622532+tahirgngr1@users.noreply.github.com>
2023-02-21 16:14:53 +0300

    .

---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList myList = new MyList();
+            myList.Ekle();
+        }
+    }
+    public class MyList
+    {
+        Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+        public void Ekle()
+        {
+            myDictionary.Add(1, "Tahir");
+            myDictionary.Add(2, "Kenan");
+            myDictionary.Add(3, "Ahmet");
+            myDictionary.Add(4, "Mehmet");
+            foreach (KeyValuePair<int, string> kvp in myDictionary)
+            {
+                Console.WriteLine(kvp);
+            }
+
+        }    
+    }
+
+}
